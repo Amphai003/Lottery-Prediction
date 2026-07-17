cbc6b060ad0b10be54abb635affdedae1cd3aca4
Amphai003 <Amphaiismyname@gmail.com>
2026-07-17 14:38:28 +0700

    feat: implement inline prize calculation and result analysis within the registry component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import Header from './components/Header'
 import Analytics from './components/Analytics'
 import PredictionNodes from './components/PredictionNodes'
 import Registry from './components/Registry'
-import PrizeCalculator from './components/PrizeCalculator'
+
 
 function App() {
   const dispatch = useDispatch()
@@ -239,8 +239,6 @@ function App() {
       />
 
       <Analytics stats={stats} />
-
-      <PrizeCalculator history={history} />
 
       <PredictionNodes 
         gptSets={gptSets} geminiSets={geminiSets} localSets={localSets} autoSets={autoSets}
